cdaafa9096c1f02bfe30c0661f9c24c076c120fa
HP <venkatkrish50000@gmail.com>
2026-06-10 19:25:27 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/test/question_service/QuestionController.java
+++ src/main/java/com/test/question_service/QuestionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QuestionController {
 
-    @GetMapping("/")
+    @GetMapping("api")
     public String addQuestion(){
         return "This is for Docker test! updated.....!!!!WOW ";
     }
