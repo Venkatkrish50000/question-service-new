24311a4477108f5d48d14b275d2e2acc8cea4346
HP <venkatkrish50000@gmail.com>
2026-06-11 16:42:04 +0530

    update fix

--- src/main/java/com/test/question_service/QuestionServiceApplication.java
+++ src/main/java/com/test/question_service/QuestionServiceApplication.java
@@ -8,6 +8,8 @@ public class QuestionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionServiceApplication.class, args);
+
+		//this is test push....
 	}
 
 }
